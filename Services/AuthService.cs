2ec845d395032be404c5d343854d8d736a2c791e
Abdelrahman4542 <helmyabdo4545@gmail.com>
2026-05-26 01:35:59 +0300

    Initial WPF MVVM setup with SQL connection

---
+++ Services/AuthService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using StoreManagementSystem.Helpers;
+
+namespace StoreManagementSystem.Services
+{
+    public class AuthService
+    {
+        public bool TestConnection()
+        {
+            try
+            {
+                using SqlConnection connection = DbConnection.GetConnection();
+
+            connection.Open();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+
+}
---
+++ Views/LoginWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using StoreManagementSystem.Services;
+
+namespace StoreManagementSystem.Views
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+
+        AuthService authService = new AuthService();
+
+            bool isConnected = authService.TestConnection();
+
+            MessageBox.Show(
+                isConnected
+                ? "Database Connected Successfully"
+                : "Database Connection Failed"
+            );
+        }
+    }
+
+}
